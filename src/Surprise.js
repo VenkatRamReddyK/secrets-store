b646be69952be705f85871815c25edc488a76a02
Venkat Ram Reddy Kunta <venkatramreddykunta@gmail.com>
2021-10-11 22:48:48 -0700

    - Updated Surprise - encrypt and decrypt is done.

--- src/Surprise.js
+++ src/Surprise.js
@@ -1,23 +1,32 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-
+const Cryptr = require('cryptr');
 import useInterval from './services/useInterval.js';
 import useTimeout from './services/useTimeout.js';
 
 export default function Component() {
-  const [count, setCount] = useState(5);
-  const [delay, setDelay] = useState(1000); //10
+  const [count, setCount] = useState(3);
+  const [delay, setDelay] = useState(null); //3
   const [visible, setVisible] = useState(false);
+  const [passcode, setPasscode] = useState("");
   const [imageSrc, setImageSrc] = useState(null);
+  const [gender, setGender] = useState(null);
+  const [error, setError] = useState(null);
 
   useInterval(() => {
-    setCount(count - 1);
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      setDelay(null);
+      // getData();      
+    }
+
   }, delay);
 
-  useTimeout(() => {
-    setDelay(null);
-    getData();
-  }, 5000);
+  // useTimeout(() => {
+  //   setDelay(null);
+  //   getData();
+  // }, 5000);
 
   const getData = () => {
     console.log('reading...');
@@ -25,6 +34,27 @@ export default function Component() {
       'https://gender-reveals.s3.amazonaws.com/data/data.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVOEG5XWC35GBSXXY%2F20211010%2Fus-west-1%2Fs3%2Faws4_request&X-Amz-Date=20211010T155423Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=f6629ac618871e1d6a28cb23c02498e3874e1793e6c6033eb44f161407211639';
     axios.get(url).then((response) => {
       console.log('response: ', response.data);
+      try {
+        
+      let cryptr = new Cryptr(passcode);
+      // const encryptedPasscode = cryptr.encrypt(passcode);
+      const decryptedPasscode = cryptr.decrypt(response.data.passcode);
+      // console.log("Input encrypted passcode: ", encryptedPasscode);
+      console.log("Ciper passcode: ", response.data.passcode);
+      
+      if(decryptedPasscode === passcode) {
+        const decryptedString = cryptr.decrypt(response.data.gender);
+        setGender(decryptedString);
+        // const decryptedString = cryptr.decrypt(response.data.gender);
+        setDelay(1000);        
+      } else {
+        setError("Invalid passcode");
+      }
+    }
+    catch(error) {
+      setError("Invalid passcode");
+    };
+    
     });
   };
   // const getDecryptedGender = (encryptedGender, inputPasscode) => {
@@ -40,9 +70,16 @@ export default function Component() {
     // let data = { gender: gender, passcode: passCode };
     let url = 'https://gender-reveals.s3.amazonaws.com/data/data.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVOEG5XWC35GBSXXY%2F20211010%2Fus-west-1%2Fs3%2Faws4_request&X-Amz-Date=20211010T155502Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=de41201250367819b4f7ec44d3e3aa6690b53d938b7933bde8d82d06b8427b84'
     axios.put(url, data).then((response) => {
-      console.log('response: ', response);
+      console.log('response: ', response.passcode);      
     });
   };
+  const onChange = (input) => {
+    setPasscode(input);
+  }
+
+  const submitPasscode = () => {
+    getData();    
+  }
 
   return (
     <>
@@ -51,11 +88,31 @@ export default function Component() {
           className="reveal-image"
           src="https://secrets-store.vercel.app/tadi_family.jpeg"
         /> */}
+        {!visible && (
+          <>
+            <p>Do you wan't to reveal the gender ? </p>
+            <button onClick={() => setVisible(true)}>Reveal</button>
+          </>
+        )}
 
-        <button onClick={() => saveData()}>Save Data</button> */}
+        {
+          visible && (
+            <>
+              Please Enter Passcode <input type="text" onChange={(e) => onChange(e.target.value)} />
+              <button onClick={() => submitPasscode()}>Submit</button>
+            </>
+          )}
+        {/* <button onClick={() => saveData()}>Save Data</button> */} */}
         <h1>{count}</h1>
-        {visible && <p>Hurrey It's a boy</p>}
-        <button onClick={() => getData()}>Get Data</button>
+          {visible && !!gender && !error && !delay && (
+            <p>Hurrey It's a {gender}</p>
+          )}
+          {error && (
+            <p>
+              {error}
+            </p>
+          )}
+        {/* <button onClick={() => getData()}>Get Data</button> */}
       </div>
     </>
   );
